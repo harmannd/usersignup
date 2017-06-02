b2c8d6ff7327b877490204bf72b8817a7da5039c
harmannd <derekhar2468@gmail.com>
2017-06-02 13:41:26 -0500

    added hashing to username, sent through cookie

--- main.py
+++ main.py
@@ -2,10 +2,24 @@ import os
 import jinja2
 import webapp2
 import re
+import hmac
+
+SECRET = 'imsosecret'
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
+
+def hash_str(s):
+    return hmac.new(SECRET, s).hexdigest()
+
+def make_secure_val(s):
+    return "{}|{}".format(s, hash_str(s))
+
+def check_secure_val(h):
+    val = h.split('|')[0]
+    if h == make_secure_val(val):
+        return val
 
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 PASSWORD_RE = re.compile(r"^.{3,20}$")
@@ -66,15 +80,16 @@ class MainPage(Handler):
         if error:
             self.render('signup.html', **params)
         else:
-            self.redirect('/welcome?username=' + username)
+            self.response.headers.add_header('Set-Cookie', 'user_id={}'.format(make_secure_val(username)))
+            self.redirect('/welcome')
 
 
 class WelcomeHandler(Handler):
     def get(self):
-        username = self.request.get('username')
+        username = self.request.cookies.get('user_id').split('|')[0]
         self.render('welcome.html', username = username)
 
 
-app = webapp2.WSGIApplication([('/', MainPage),
+app = webapp2.WSGIApplication([('/signup', MainPage),
                                ('/welcome', WelcomeHandler)],
                                 debug=True)
